7c47fdf645bc60fcd958028bbdac6363106da429
Maria Clara <maria.c.ferreira47@aluno.senai.br>
2025-05-16 11:41:07 -0300

    Cria o arquivo modelo02.js e multiplica os numeros

--- modelo.02.js
+++ modelo.02.js
@@ -3,4 +3,5 @@ let numero2 = 4;
 
 let multiplicacao = numero1 * numero2;
 
-console.log (`A multiplicação de ${numero1} e ${numero2} é ${multiplicacao}
+console.log (`A multiplicação de ${numero1} e ${numero2} é ${multiplicacao}`)
+
